893dafda37b08df2ada925daac802301f5e7bbb6
zakaria <52750533+zcubbs@users.noreply.github.com>
2023-07-23 18:02:54 +0200

    :sparkles: update k3s cmd

--- cmd/k3s/install.go
+++ cmd/k3s/install.go
@@ -10,13 +10,25 @@ import (
 	"log"
 )
 
+var (
+	disable             []string
+	dataDir             string
+	volumeStorageDir    string
+	writeKubeconfigMode string
+)
+
 // install represents the list command
 var install = &cobra.Command{
 	Use:   "install",
 	Short: "install k3s",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := k3s.Install()
+		err := k3s.Install(k3s.Config{
+			Disable:                 disable,
+			DataDir:                 dataDir,
+			DefaultLocalStoragePath: volumeStorageDir,
+			WriteKubeconfigMode:     writeKubeconfigMode,
+		})
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -24,5 +36,9 @@ var install = &cobra.Command{
 }
 
 func init() {
+	install.Flags().StringArrayVarP(&disable, "disable", "dis", nil, "list of k3s features to disable")
+	install.Flags().StringVarP(&dataDir, "dataDir", "dir", "", "data storage directory")
+	install.Flags().StringVarP(&volumeStorageDir, "volumeStorageDir", "vs", "", "volume storage directory")
+	install.Flags().StringVarP(&writeKubeconfigMode, "writeKubeconfigMode", "wm", "", "write kubeconfig mode")
 	Cmd.AddCommand(install)
 }
